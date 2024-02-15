5b9ee2c0f8e1985c7cd6c9083b00d779b5eac7a7
HebaZaki <h_borhan@hotmail.com>
2024-02-15 18:51:09 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -32,8 +32,7 @@ void selection_sort(int *array, size_t size)
             temp = array[min_index];
             array[min_index] = array[i];
             array[i] = temp;
+            print_array(array, size);
         }
-    
-        print_array(array, size);
     }
 }
