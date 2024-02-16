f8a73cafbef0e7044f061bbd90db64af0577ed06
HebaZaki <h_borhan@hotmail.com>
2024-02-16 09:21:08 +0200

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,28 +11,28 @@
 
 void selection_sort(int *array, size_t size)
 {
-    int temp;
-    size_t i, j, min_index;
+int temp;
+size_t i, j, min_index;
 
-    if (size < 2)
-        return;
+if (size < 2)
+return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
+for (i = 0; i < size - 1; i++)
+{
+min_index = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+min_index = j;
+}
 
-        if (min_index != i)
-        {
-            temp = array[min_index];
-            array[min_index] = array[i];
-            array[i] = temp;
-            print_array(array, size);
-        }
-    }
+if (min_index != i)
+{
+temp = array[min_index];
+array[min_index] = array[i];
+array[i] = temp;
+print_array(array, size);
+}
+}
 }
