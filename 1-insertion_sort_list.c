158924865a1e5c3cfa1b903eb3849fea0895f115
Heba <hebamagde@gmail.com>
2024-02-18 18:54:44 +0200

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,26 +8,26 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *current, *temp, *node;
-if (*list == NULL || (*list)->next == NULL || list == NULL)
+listint_t *current, *compare, *temp;
+if (*list == NULL || (*list)->next == NULL)
 return;
-for (node = (*list)->next; node != NULL; node = temp)
+for (current = (*list)->next; current != NULL; current = temp)
 {
-temp = node->next;
-current = node->prev;
-while (current != NULL && node->n < current->n)
+temp = current->next;
+compare = current->prev;
+while (compare != NULL && current->n < compare->n)
 {
-current->next = node->next;
-if (node->next != NULL)
-node->next->prev = current;
-node->prev = current->prev;
-node->next = current;
-if (current->prev != NULL)
-current->prev->next = node;
+compare->next = current->next;
+if (current->next != NULL)
+current->next->prev = compare;
+current->prev = compare->prev;
+current->next = compare;
+if (compare->prev != NULL)
+compare->prev->next = current;
 else
-*list = node;
-current->prev = node;
-current = node->prev;
+*list = current;
+compare->prev = current;
+compare = compare->prev->prev;
 print_list(*list);
 }
 }
---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * quick_sort - function that sorts an array of integers
+ * @array: pointer to the first element of an integer array
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+
+}
