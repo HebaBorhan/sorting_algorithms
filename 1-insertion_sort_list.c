92db0e75be8a4b2386c679d029a2bc53ae93307a
Heba <hebamagde@gmail.com>
2024-02-18 12:41:37 +0200

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,25 +8,25 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *current, *temp, *node;
+listint_t **current, *temp, *node;
 if (*list == NULL || (*list)->next == NULL || list == NULL)
 return;
 for (node = (*list)->next; node != NULL; node = temp)
 {
 temp = node->next;
 current = node->prev;
-while (current != NULL && node->n < current->n)
+while (current != NULL && node->n < (*current)->n)
 {
-current->next = node->next;
+(*current)->next = node->next;
 if (node->next != NULL)
 node->next->prev = current;
-node->prev = current->prev;
+node->prev = (*current)->prev;
 node->next = current;
-if (current->prev != NULL)
-current->prev->next = node;
+if ((*current)->prev != NULL)
+(*current)->prev->next = node;
 else
 *list = node;
-current->prev = node;
+(*current)->prev = node;
 current = node->prev;
 print_list(*list);
 }
